fa9670a1d990f76296a33a0f8f2848dcde4a9aae
Heya Sharma <heyaasharma@gmail.com>
2024-04-18 15:49:53 +0530

    changes made

--- src/Components/Customized.jsx
+++ src/Components/Customized.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 export default function Customized() {
   return (
     <div className='cus'>
-      <h3 style={{margin:"0"}}>Specially customized gifts for your loved ones with &hearts;</h3>
-      <img src="mac8.jpg"></img>  
-         {/* <h3>Specially customized gifts for your loved ones with &hearts;</h3> */}
+      <h3 style={{margin:"0",fontSize:"30px"}}>Personalize your gift-giving experience with our customizable macaroon options, tailored to suit any occasion or recipient. From custom flavors to elegant packaging, create a sweet and unforgettable present they'll cherish.Surprise your  loved ones with &hearts;</h3>
+      <img src="mac8.jpg"></img> 
       <img src="mac1.jpg"></img>
     </div>
   )
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className='h'>
       <img src="mac1.jpg"></img>
       <img src="mac3.jpg"></img>
-      <p style={{margin:"0"}}>Enjoy and Endulge in happiness and joys of colorful macroons for life...&hearts;</p>
+      <p style={{margin:"0", fontSize: "20px"}}>Indulge in a symphony of flavors with our exquisite macarons, each delicately crafted to perfection. Explore our online bakery for a delectable journey of sweet delights....&hearts;</p>
       <img src="mac9.jpg"></img>
       <img src="mac5.jpg"></img>
       
