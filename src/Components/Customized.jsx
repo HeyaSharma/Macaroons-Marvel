ffbe9558d41876024cbb7fea7f282fb8444d31c7
Heya Sharma <heyaasharma@gmail.com>
2024-04-18 15:37:35 +0530

    changes made

--- src/Components/Customized.jsx
+++ src/Components/Customized.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Customized() {
   return (
     <div className='cus'>
-      <h3>Specially customized gifts for your loved ones with &hearts;</h3>
+      <h3 style={{margin:"0"}}>Specially customized gifts for your loved ones with &hearts;</h3>
       <img src="mac8.jpg"></img>  
          {/* <h3>Specially customized gifts for your loved ones with &hearts;</h3> */}
       <img src="mac1.jpg"></img>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className='h'>
       <img src="mac1.jpg"></img>
-      <img src="mac2.jpg"></img>
-      <p>Enjoy and Endulge in happiness and joys of colorful macroons for life...&hearts;</p>
-      <img src="mac9.jpg"></img>
       <img src="mac3.jpg"></img>
+      <p style={{margin:"0"}}>Enjoy and Endulge in happiness and joys of colorful macroons for life...&hearts;</p>
+      <img src="mac9.jpg"></img>
+      <img src="mac5.jpg"></img>
       
     </div>
   )
